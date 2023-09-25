a96b5111995ed868395e9050dcf6b148a2ad5b1f
akaryaka <a.karyaka.2dit@gmail.com>
2023-09-26 01:11:23 +0600

    update todo, begin timer

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,3 +1,22 @@
 //логика: 
-//- добавляем с помощью интервала секунды/минуты/часы
 //- если доходят до крайней цифры, то обнуляем и добавляем +1 к следующему разряду
+const start = document.getElementById('start-btn');
+let seconds = document.querySelector('.seconds');
+let minutes = document.querySelector('.minutes');
+let hours = document.querySelector('.hours');
+
+function timer() {
+  let secondsCounter = 0;
+
+  function timeIncrement() {
+    secondsCounter = secondsCounter+1; 
+    seconds.innerHTML = secondsCounter;
+
+    if(secondsCounter == 59) {
+      secondsCounter = -1;
+    }
+  }
+  setInterval(timeIncrement, 1000)
+}
+
+start.addEventListener('click', timer);
--- scripts/todo.js
+++ scripts/todo.js
@@ -9,14 +9,17 @@ function addTodo() {
   todoItem.innerHTML = `${input.value}<button class="delete-btn btn">удалить</button><button class="done-btn btn">выполненно</button>`;
   todoItem.classList.add('list__item');
   list.appendChild(todoItem);
+
+  let deleteBtn = document.querySelectorAll('.delete-btn');
+  for (let i=0; i<deleteBtn.length; i++){
+    if(deleteBtn[i]) {
+      deleteBtn[i].addEventListener('click', function(event) {
+        let targetBtn = event.target;
+        let parent = targetBtn.parentNode;
+        parent.style.display = 'none';
+      })
+    }
+  }  
 }
 
 addBtn.addEventListener('click', addTodo);
-
-const deleteBtn = document.querySelector('.delete-btn');
-if(deleteBtn) {
-  deleteBtn.addEventListener('click', function(event) {
-  console.log(event.target);
-})
-}
-
