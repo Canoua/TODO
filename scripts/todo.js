eb9cf0550aa58ab69cad533bc726f8b5f57e6830
Andrew-Karyaka <akaryaka1997@gmail.com>
2023-09-25 22:48:19 +0600

    add todo

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,2 +1,22 @@
-console.log('todo');
+const addBtn = document.getElementById('add-btn');
+const formInput = document.getElementById('form-input');
+const list = document.getElementById('list');
+const input = document.getElementById('input');
+
+function addTodo() {
+  const todoItem = document.createElement('div');
+  
+  todoItem.innerHTML = `${input.value}<button class="delete-btn btn">удалить</button><button class="done-btn btn">выполненно</button>`;
+  todoItem.classList.add('list__item');
+  list.appendChild(todoItem);
+}
+
+addBtn.addEventListener('click', addTodo);
+
+const deleteBtn = document.querySelector('.delete-btn');
+if(deleteBtn) {
+  deleteBtn.addEventListener('click', function(event) {
+  console.log(event.target);
+})
+}
 
