c0d55a2b4d1869450bae3c61aef973138ee0f182
akaryaka <a.karyaka.2dit@gmail.com>
2023-10-18 01:42:56 +0600

    edit

--- src/js/components/todo/script.js
+++ src/js/components/todo/script.js
@@ -9,7 +9,7 @@ function addTodo() {
   //рендер дела/рефакторинг//
   todoItem.classList.add('list__item');
   todoItem.innerHTML = `
-    ${input.value}
+    <input class="todo-item__input" value=${input.value} readonly>
     <div class="btn-wrapper">
       <button class="delete-btn btn">x</button>
       <button class="edit-btn btn">
@@ -25,14 +25,20 @@ function addTodo() {
   function validation() {
     if(input.value !== '') {
       const list = document.getElementById('list');
+      const editBtn = document.querySelectorAll('.edit-btn');
 
       list.appendChild(todoItem);
-      const editBtn = document.querySelectorAll('.edit-btn');
       for(let i=0; i<editBtn.length; i++) {
-        // editBtn[i].addEventListener('click', function(event) {
-          
-        // })
+        editBtn[i].addEventListener('click', function(event) {
+          let targetBtn = event.target;
+          let listItem = targetBtn.closest('.list__item');
+          let todoItemInput = listItem.querySelector('.todo-item__input');
+          todoItemInput.removeAttribute('readonly');
+          todoItemInput.style.borderColor='#000';
+          todoItemInput.focus();
+        })
       }
+      
       input.value = '';
       error.innerHTML = '';
       stub.innerHTML = '';
@@ -52,29 +58,24 @@ function addTodo() {
 
       //добавляем заглушку
       if(i==0) {
-        stub.innerHTML = 'Дел нет';
+        stub.innerHTML = `
+          Дел нет
+          <img class="list-done__stub-img" src="https://utyug.info/upload/medialibrary/3f9/3f9bd29509b04a7aed1b4b90b0099277.jpg">
+        `;
       }
     })
   }
 
   //фиксация выполнения
   let doneBtn = document.querySelectorAll('.done-btn');
-  // for(let i=0; i<doneBtn.length; i++) {
-  //   doneBtn[i].addEventListener('click', function(event) {
-  //     let targetBtn = event.target;
-  //     let parentDone = targetBtn.closest('.list__item');
-  //     let doneList = document.querySelector('.list-done');
-  //     let doneStub = document.querySelector('.list-done__stub');
+  for(let i=0; i<doneBtn.length; i++) {
+    doneBtn[i].addEventListener('click', function(event) {
+      let targetBtn = event.target;
+      let listItem = targetBtn.closest('.list__item');
 
-  //     if(i==0) {
-  //       stub.innerHTML = 'Дел нет';
-  //     } 
-
-      // doneStub.style.display = 'none';
-      
-      // doneList.appendChild(parentDone);
-    // })
-  // }
+      // console.log(listItem.querySelector('.todo-item__input'));
+    })
+  }
 }
 
 addBtn.addEventListener('click', addTodo);
