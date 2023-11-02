d2e8f0608c62b3cd2a10a998128cf54c785e7dc4
akaryaka <a.karyaka.2dit@gmail.com>
2023-11-02 16:20:37 +0600

    ref structure

--- src/js/components/todo/modules/actions.js
+++ src/js/components/todo/modules/actions.js
@@ -1,6 +1,25 @@
-export default function editTodo() {
+export default function actions() {
+  let deleteBtns = document.querySelectorAll('.delete-btn');
   let editBtns = document.querySelectorAll('.edit-btn');
+  let doneBtns = document.querySelectorAll('.done-btn');
   
+  // удаление дела
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener('click', function(event) {
+      let targetBtn = event.target;
+      let parent = targetBtn.closest('.list__item');
+      let listClass = document.querySelector('.list');
+  
+      parent.remove();
+     
+      // добавляем отображение заглушки при отсутсвии дел
+      if (listClass.children.length < 1) {
+        stub.classList.remove('stub-none');
+      }
+    })
+  })
+
+  // редактирование дела
   for (let editBtn of editBtns) {
     editBtn.addEventListener('click', function(event) {
       // определяем элемент, по которому кликаем
@@ -34,6 +53,24 @@ export default function editTodo() {
           todoItemInput.style.borderColor='transparent';
           confirmBtn.style.display = 'none';
         })
+      }
+    })
+  }
+
+
+  // подтверждение дела
+  for (let doneBtn of doneBtns) {
+    doneBtn.addEventListener('click', function(event) {
+      let targetBtn = event.target;
+      let listItem = targetBtn.closest('.list__item');
+      let doneBtnsClick = document.querySelectorAll('.done-click');
+      
+      listItem.classList.add('list__item_done');
+
+      // refactoring
+      for (let doneBtnClick of doneBtnsClick) {
+        doneBtnClick.setAttribute('disabled', 'disabled');
+        doneBtnClick.style.cursor = 'inherit';
       }
     })
   }
--- src/js/components/todo/modules/addTodo.js
+++ src/js/components/todo/modules/addTodo.js
@@ -1,7 +1,8 @@
 import todoItemHtml from "../render/todoItemHtml";
-import deleteTodo from "./actions/deleteTodo";
-import editTodo from "./actions/editTodo";
-import doneTodo from "./actions/doneTodo";
+// import deleteTodo from "./actions/deleteTodo";
+// import editTodo from "./actions/editTodo";
+// import doneTodo from "./actions/doneTodo";
+import actions from "./actions";
 import animation from "./animation";
 
 export default function addTodo() {
@@ -13,12 +14,8 @@ export default function addTodo() {
     todoItemHtml();
     // animation
     animation();
-    // удаление дела по кнопке "x"
-    deleteTodo();
-    // функция кнопки редактирования
-    editTodo();
-    // фиксация выполнения
-    doneTodo();
+    // кнопки
+    actions();
   }
   addBtn.addEventListener('click', adding); 
 }
