71ce98130fbe915f44932d3c3bbff60b6eaf5647
akaryaka <a.karyaka.2dit@gmail.com>
2023-11-03 23:42:59 +0600

    edit fixed

--- src/js/components/todo/todoScript/modules/actions.js
+++ src/js/components/todo/todoScript/modules/actions.js
@@ -1,11 +1,10 @@
 export default function actions() {
-  let editBtns = document.querySelectorAll('.edit-btn');
   let listClass = document.querySelector('.list');
   
   function deleteTodo(event) {
     let deleteElem = event.target;
 
-     // удаляем дело по клике на кнопку
+    // удаляем дело по клике на кнопку
     if (deleteElem.dataset.action === 'delete') {
       deleteElem.closest('.list__item').remove();
     }
@@ -18,54 +17,39 @@ export default function actions() {
 
   // редактирование дела
   function editTodo(event) {
+    // определяем элемент, по которому кликаем
     let editBtn = event.target;
-    let listItem = editBtn.closest('.list__item');
-    let todoItemInputWrapper = listItem.querySelector('.todo-item__input-wrapper');
-    let confirmBtn = todoItemInputWrapper.querySelector('.todo-item__confirm'); 
 
-    editBtn.setAttribute('disabled', 'disabled');
-    todoItemInputWrapper.style.border='1px solid #000';
-    confirmBtn.style.display = 'block';
+    if(editBtn.dataset.action === 'edit') {
+      // находим обертку нашего дела
+      let listItem = editBtn.closest('.list__item');
+      let todoItemInputWrapper = listItem.querySelector('.todo-item__input-wrapper');
+      let confirmBtn = todoItemInputWrapper.querySelector('.todo-item__confirm'); 
+      // инпут для редактирования дела
+      let todoItemInput = listItem.querySelector('.todo-item__input');
+
+      // делаем кнопку edit не кликабельной
+      editBtn.setAttribute('disabled', 'disabled');
+      // добавляем бордер на обертку инпута
+      todoItemInputWrapper.style.border='1px solid #000';
+      // добавляем возможность редактировани дела
+      todoItemInput.removeAttribute('readonly');
+      // фокус на инпуте
+      todoItemInput.focus();
+
+      // отображаем кнопку подтверждения
+      confirmBtn.style.display = 'block';
+
+      confirmBtn.addEventListener('click', function() {
+        editBtn.removeAttribute('disabled', 'disabled');
+        todoItemInputWrapper.style.border='none';
+        todoItemInput.setAttribute('readonly', 'readonly');
+          todoItemInput.style.borderColor='transparent';
+          confirmBtn.style.display = 'none';
+      })
+    }
   }
 
-  // for (let editBtn of editBtns) {
-    // editBtn.addEventListener('click', function(event) {
-      // определяем элемент, по которому кликаем
-      // let targetEditBtn = event.target;
-      // находим обертку нашего дела
-      // let listItem = targetEditBtn.closest('.list__item');
-      // инпут для редактирования дела
-      // let todoItemInput = listItem.querySelector('.todo-item__input');
-      
-      // let todoItemInputWrapper = listItem.querySelector('.todo-item__input-wrapper');
-      // let confirmBtns = todoItemInputWrapper.querySelectorAll('.todo-item__confirm');
-      // let editBtnsClick = document.querySelectorAll('.edit-click');
-
-      // for (let editBtnClick of editBtnsClick) {
-      //   editBtnClick.setAttribute('disabled', 'disabled');
-      //   todoItemInputWrapper.style.border='1px solid #000';
-      // }
-
-
-      // ***
-      // todoItemInput.removeAttribute('readonly');
-      // todoItemInput.style.borderColor='#000';
-      // todoItemInput.focus();
-     
-      // for (let confirmBtn of confirmBtns) {
-      //   confirmBtn.style.display = 'block';
-      //   confirmBtn.addEventListener('click', function() {
-      //     for (let editBtnClick of editBtnsClick) {
-      //       editBtnClick.removeAttribute('disabled', 'disabled');
-      //       todoItemInputWrapper.style.border='none';
-      //     }
-      //     todoItemInput.setAttribute('readonly', 'readonly');
-      //     todoItemInput.style.borderColor='transparent';
-      //     confirmBtn.style.display = 'none';
-        // })
-      // }
-    // })
-  // }
 
   function doneTodo(event) {
     let doneElem = event.target;
