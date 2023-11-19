957f8b9584b64597859ae72cc88d9ff75ca9c0db
akaryaka <a.karyaka.2dit@gmail.com>
2023-11-19 14:48:01 +0600

    begin ref

--- src/js/components/todo/todoScript/todoScript.js
+++ src/js/components/todo/todoScript/todoScript.js
@@ -87,11 +87,12 @@ export default function todoScript() {
     // добавляем массив в локалсторадж
     localStorage.setItem('Массив дел', JSON.stringify(tasks));
   }
-
+  
   // удаление дела
   function deleteTodo(event) {
     let deleteElem = event.target;
     let listItemId =  deleteElem.closest('.list__item').id;
+
     // ищем индекс удаляемого дела
     const deleteTaskFromArr =  tasks.findIndex(function(task) {
       if(task.id == listItemId) {
@@ -151,76 +152,97 @@ export default function todoScript() {
   }
 
   // редактирование дела
-  // function editTodo(event) {
-  //   // определяем элемент, по которому кликаем
-  //   let editBtn = event.target;
+  function editTodo(event) {
+    // определяем элемент, по которому кликаем
+    let editBtn = event.target;
 
-  //   if(editBtn.dataset.action === 'edit') {
-  //     // находим обертку нашего дела
-  //     let listItem = editBtn.closest('.list__item');
-  //     // обертка с кнопками
-  //     let btnWrapper = listItem.querySelector('.btn-wrapper');
-  //     let todoItemInputWrapper = listItem.querySelector('.todo-item__input-wrapper');
-  //     // кнопка подтверждения
-  //     let confirmBtn = todoItemInputWrapper.querySelector('.todo-item__confirm'); 
-  //     // инпут для редактирования дела
-  //     let todoItemInput = listItem.querySelector('.todo-item__input');
+    if(editBtn.dataset.action === 'edit') {
+      // находим обертку нашего дела
+      let listItem = editBtn.closest('.list__item');
+      // обертка с кнопками
+      let btnWrapper = listItem.querySelector('.btn-wrapper');
+      let todoItemInputWrapper = listItem.querySelector('.todo-item__input-wrapper');
+      // кнопка подтверждения
+      let confirmBtn = todoItemInputWrapper.querySelector('.todo-item__confirm'); 
+      // инпут для редактирования дела
+      let todoItemInput = listItem.querySelector('.todo-item__input');
 
-  //     // плавное исчезновение
-  //     let fadeBtns = new Promise(function() {
-  //       setTimeout(() => {
-  //         btnWrapper.classList.add('fade');
-  //       }, 300)
-  //     });
+      // плавное исчезновение
+      let fadeBtns = new Promise(function() {
+        setTimeout(() => {
+          btnWrapper.classList.add('fade');
+        }, 300)
+      });
 
-  //     // убираем блок с кнопками
-  //     let invisibleBtns = new Promise(() => {
-  //       setTimeout(() => {
-  //         btnWrapper.style.height='0';
-  //         btnWrapper.style.width='0';
-  //       }, 700)
-  //     })
+      // убираем блок с кнопками
+      let invisibleBtns = new Promise(() => {
+        setTimeout(() => {
+          btnWrapper.style.height='0';
+          btnWrapper.style.width='0';
+        }, 700)
+      })
 
-  //     let inputWrapperEdit = new Promise(() => {
-  //       setTimeout(() => {
-  //         // добавляем бордер на обертку инпута
-  //         todoItemInputWrapper.style.borderColor = '#000';
-  //         // расширяем контейнер с инпутом
-  //         todoItemInputWrapper.style.width = '100%';
+      let inputWrapperEdit = new Promise(() => {
+        setTimeout(() => {
+          // добавляем бордер на обертку инпута
+          todoItemInputWrapper.style.borderColor = '#000';
+          // расширяем контейнер с инпутом
+          todoItemInputWrapper.style.width = '100%';
 
-  //         todoItemInput.style.width = '90%';
-  //         // добавляем возможность редактировани дела
-  //         todoItemInput.removeAttribute('readonly');
-  //         // фокус на инпуте
-  //         todoItemInput.focus();
+          todoItemInput.style.width = '90%';
+          // добавляем возможность редактировани дела
+          todoItemInput.removeAttribute('readonly');
+          // фокус на инпуте
+          todoItemInput.focus();
 
-  //         confirmBtn.style.width = '10%';
-  //         // отображаем кнопку подтверждения
-  //         confirmBtn.style.display = 'block';
-  //       }, 1000)
-  //     })
+          confirmBtn.style.width = '10%';
+          // отображаем кнопку подтверждения
+          confirmBtn.style.display = 'block';
+        }, 1000)
+      })
 
-  //     confirmBtn.addEventListener('click', function() {
-  //       // возвращаем контейнер в прежнее состояние
-  //       todoItemInputWrapper.style.borderColor = 'transparent';
-  //       todoItemInputWrapper.style.width = 'auto';
+      // let editElem = event.target;
+        let listItemId =  editBtn.closest('.list__item').id;
+        // ищем индекс удаляемого дела
+        const editTaskFromArr =  tasks.findIndex(function(task) {
+          if(task.id == listItemId) {
+            return true;
+          }
+        });
+        console.log(editTaskFromArr);
+
+      confirmBtn.addEventListener('click', function() {
+        // возвращаем контейнер в прежнее состояние
+        todoItemInputWrapper.style.borderColor = 'transparent';
+        todoItemInputWrapper.style.width = 'auto';
         
-  //       // отображаем обертку с кнопками
-  //       btnWrapper.classList.remove('fade');
-  //       btnWrapper.style.height='auto';  
-  //       btnWrapper.style.width='auto';
+        // отображаем обертку с кнопками
+        btnWrapper.classList.remove('fade');
+        btnWrapper.style.height='auto';  
+        btnWrapper.style.width='auto';
         
-  //       // убираем возможность редактирования дела
-  //       todoItemInput.setAttribute('readonly', 'readonly');
-  //       todoItemInput.style.width = 'auto';
+        // убираем возможность редактирования дела
+        todoItemInput.setAttribute('readonly', 'readonly');
+        todoItemInput.style.width = 'auto';
 
-  //       // скрываем кнопку подтверждения 
-  //       confirmBtn.style.display = 'none';
-  //     })
-  //   }
-  // }
+        // скрываем кнопку подтверждения 
+        confirmBtn.style.display = 'none';
+
+    
+        
+        // удаляем дело по клике на кнопку
+        // if (deleteElem.dataset.action === 'delete') {
+          // deleteElem.closest('.list__item').remove();
+          // удаляем дело из массива
+          // tasks.splice(deleteTaskFromArr, 1);
+        // }
+        // localStorage.setItem('Массив дел', JSON.stringify(tasks));
+      })
+    }
+  }
 
   form.addEventListener('submit', adding); 
+  list.addEventListener('click', editTodo);
   list.addEventListener('click', deleteTodo);
   list.addEventListener('click', doneTodo);
 }
